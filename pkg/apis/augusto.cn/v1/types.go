0703068ec72fead18a3052339a1809fc78013435
Augusto Yao <staugusto91@gmail.com>
2018-08-14 19:03:39 +0800

    define crd

--- pkg/apis/augusto.cn/v1/types.go
+++ pkg/apis/augusto.cn/v1/types.go
@@ -11,11 +11,16 @@ type EsCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	Spec              EsClusterSpec `json:"spce"`
+	Status            EsStatus `json:"status"`
 }
 
 type EsClusterSpec struct {
 	Message   string `json:"message"`
 	SomeValue *int32 `json:"someValue"`
+}
+
+type EsStatus struct {
+	state string `json:"state"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
