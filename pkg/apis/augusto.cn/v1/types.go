87e498612f3fdd1e260769de08b793637289024d
Augusto Yao <staugusto91@gmail.com>
2018-08-15 10:54:05 +0800

    right types.go

--- pkg/apis/augusto.cn/v1/types.go
+++ pkg/apis/augusto.cn/v1/types.go
@@ -3,27 +3,33 @@ package v1
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-
+// Foo is a specification for a Foo resource
 type EsCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              EsClusterSpec `json:"spce"`
-	Status            EsStatus `json:"status"`
+
+	Spec   EsClusterSpec   `json:"spec"`
+	Status EsClusterStatus `json:"status"`
 }
 
+// FooSpec is the spec for a Foo resource
 type EsClusterSpec struct {
 	Message   string `json:"message"`
 	SomeValue *int32 `json:"someValue"`
 }
 
-type EsStatus struct {
-	state string `json:"state"`
+// FooStatus is the status for a Foo resource
+type EsClusterStatus struct {
+	State string `json:"state"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// FooList is a list of Foo resources
 type EsClusterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
