9e19fc00882d2757e8d5017e0694b87f1b097161
Augusto Yao <staugusto91@gmail.com>
2018-08-16 14:29:45 +0800

    update crd definitions

--- pkg/apis/augusto.cn/v1/types.go
+++ pkg/apis/augusto.cn/v1/types.go
@@ -2,8 +2,16 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
+const (
+	SUBMITTED = "submitted"
+	INITIALIZING = "initializing"
+	RUNNING = "running"
+	RECOVERING = "recovering"
+	FAILED = "failed"
+)
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -11,28 +19,38 @@ import (
 type EsCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
+	
 	Spec   EsClusterSpec   `json:"spec"`
 	Status EsClusterStatus `json:"status"`
 }
 
 // EsClusterSpec is the spec for a EsCluster resource
 type EsClusterSpec struct {
-	Message   string `json:"message"`
-	SomeValue *int32 `json:"someValue"`
+	EsImage  string          `json:"esimage"`
+	Replicas *int32          `json:"replicas"`
+	DataPath string          `json:"datapath",omitempty`
+	Env      []corev1.EnvVar `json:"env,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,7,rep,name=env"`
 }
 
 // EsClusterStatus is the status for a EsCluster resource
 type EsClusterStatus struct {
-	State string `json:"state"`
+	Phase         string               `json:"phase"`
+	PodsStatus    []EsInstanceStatus   `json:"podsStatus"`
+	ServiceStatus corev1.ServiceStatus `json:"svcStatus"`
+}
+
+type EsInstanceStatus struct {
+	PodName     string           `json:"podName"`
+	PodHostName string           `json:"podHostName"`
+	NodeName    string           `json:"nodeName"`
+	Status      corev1.PodStatus `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // EsClusterList is a list of EsCluster resources
 type EsClusterList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata"`
-
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ListMeta   `json:"metadata"`
 	Items []EsCluster `json:"items"`
 }
