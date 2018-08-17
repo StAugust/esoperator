926f40c31a000d289f0d37fd878e02715f02efa6
Augusto Yao <staugusto91@gmail.com>
2018-08-17 09:53:47 +0800

    cmd/esoperator/main.go completed

--- cmd/esoperator/main.go
+++ cmd/esoperator/main.go
@@ -1,95 +1,63 @@
 package main
 
 import (
-	"net/http"
-	"net"
-	"github.com/Sirupsen/logrus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/heptiolabs/healthcheck"
-	"github.com/prometheus/client_golang/prometheus"
-	"os"
-	"os/signal"
-	"syscall"
-	"sync"
-	"github.com/staugust/esoperator/pkg/controller"
 	"flag"
-	"fmt"
+	"time"
+	
+	"github.com/golang/glog"
+	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	
+	clientset "github.com/staugust/esoperator/pkg/client/clientset/versioned"
+	informers "github.com/staugust/esoperator/pkg/client/informers/externalversions"
+	"github.com/staugust/esoperator/pkg/signals"
+	"github.com/staugust/esoperator/pkg/controller"
 )
 
 var (
-	appVersion = "0.1.0"
-	
-	printVersion bool
-	baseImage    string
-	kubeCfgFile  string
-	masterHost   string
+	masterURL  string
+	kubeconfig string
 )
 
-func init() {
-	flag.BoolVar(&printVersion, "version", false, "Show version and quit")
-	flag.StringVar(&baseImage, "baseImage", "skydiscovery/elasticsearch:6.2.4", "Base image to use when spinning up the elasticsearch components.")
-	flag.StringVar(&kubeCfgFile, "kubecfg-file", "", "Location of kubecfg file for access to kubernetes master service; --kube_master_url overrides the URL part of this; if neither this nor --kube_master_url are provided, defaults to service account tokens")
-	flag.StringVar(&masterHost, "masterhost", "http://127.0.0.1:6443", "Full url to k8s api server")
+func main() {
 	flag.Parse()
+	
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+	
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		glog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+	
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+	
+	esClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building example clientset: %s", err.Error())
+	}
+	
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	esInformerFactory := informers.NewSharedInformerFactory(esClient, time.Second*30)
+	
+	controller := controller.NewController(kubeClient, esClient,
+		kubeInformerFactory.Core().V1().Pods(),
+		esInformerFactory.Augusto().V1().EsClusters())
+	
+	go kubeInformerFactory.Start(stopCh)
+	go esInformerFactory.Start(stopCh)
+	
+	if err = controller.Run(2, stopCh); err != nil {
+		glog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
 
-func main() {
-	if printVersion {
-		fmt.Println("elasticsearch-operator", appVersion)
-		os.Exit(0)
-	}
-	logrus.Info("elasticsearch operator starting up!")
-	
-	// Print params configured
-	logrus.Info("Using Variables:")
-	logrus.Infof("   baseImage: %s", baseImage)
-	
-	k8sconfig, err := rest.InClusterConfig()
-	if err != nil {
-		logrus.Error("Could not find k8s config")
-		logrus.Error(err)
-		os.Exit(1)
-	}
-	
-	k8sclient, err := kubernetes.NewForConfig(k8sconfig)
-	if err != nil {
-		logrus.Error("Could not init k8sclient! ", err)
-		os.Exit(1)
-	}
-	
-	controller := controller.NewESController(k8sclient)
-	
-	doneChan := make(chan struct{})
-	var wg sync.WaitGroup
-	
-	r := prometheus.NewRegistry()
-	r.MustRegister(prometheus.NewProcessCollector(os.Getpid(), ""))
-	r.MustRegister(prometheus.NewGoCollector())
-	mux := http.NewServeMux()
-	health := healthcheck.NewMetricsHandler(r, "elasticsearch-operator")
-	mux.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
-	mux.HandleFunc("/live", health.LiveEndpoint)
-	mux.HandleFunc("/ready", health.ReadyEndpoint)
-	
-	srv := &http.Server{Handler: mux}
-	
-	l, err := net.Listen("tcp", ":8000")
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	go srv.Serve(l)
-	go controller.Run(doneChan, &wg)
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	for {
-		select {
-		case <-signalChan:
-			logrus.Error("Shutdown signal received, exiting...")
-			close(doneChan)
-			wg.Wait()
-			os.Exit(0)
-		}
-	}
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 }
